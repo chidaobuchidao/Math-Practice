1740ecfdf5a941ab85ebf8691fceec815a18dba8
chidaobuchidao <2980933590@qq.com>
2025-11-07 09:28:12 +0800

    Controller异常捕获

---
+++ src/main/java/com/mathpractice/exception/controllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.mathpractice.exception;
+
+import com.mathpractice.response.ApiResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+/*
+ 全局异常处理
+ */
+@RestControllerAdvice
+public class controllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ApiResponse<Object> handleException(Exception e) {
+        return ApiResponse.error(e.getMessage());
+    }
+}
